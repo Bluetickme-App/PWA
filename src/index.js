ba1f1cb2d8277e7d6538bb39941884566b749838
Onkar <er.onkarjanwa@gmail.com>
2021-02-09 15:44:31 +0530

    fix: minor

--- src/index.js
+++ src/index.js
@@ -42,6 +42,25 @@ const ctx = {
 const subscriptionService = new SubscriptionService(ctx);
 
 app.use(cors());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.bluetickme.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(bodyParser.json());
 
 app.get('/manifest.json', function (req, res) {
